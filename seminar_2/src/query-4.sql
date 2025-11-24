cb7eafa2f47261a2a6b8f70d0f4ed5ff20dbd27a
edwardsharp2003 <edwardsharp2003@gmail.com>
2025-11-24 17:55:09 +0100

    finished query 4

---
+++ seminar_2/src/query-4.sql
@@ -0,0 +1,33 @@
+/*
+List employee ids and names of all teachers who are allocated in more than a specific
+number of course instances during the current period.
+Table 7 below is an example result of such a query, illustrating the expected output.
+
+employee_id | teachers_name | period | number of courses
+*/
+
+
+CREATE OR REPLACE VIEW teacher_course_allocation AS
+SELECT
+    employee.employee_id,
+    person.first_name || ' ' || person.last_name AS teacher_name,
+    activity_allocation.study_period_id,
+    COUNT(DISTINCT activity_allocation.course_instance_id) AS course_count
+FROM
+    employee
+JOIN
+    person ON person.person_id = employee.person_id
+JOIN
+    activity_allocation ON activity_allocation.employee_id = employee.employee_id
+WHERE
+    activity_allocation.study_period_id = 3
+GROUP BY
+    employee.employee_id,
+    teacher_name,
+    activity_allocation.study_period_id
+
+
+
+/* to run:
+   SELECT * FROM teacher_course_allocation;
+ */
